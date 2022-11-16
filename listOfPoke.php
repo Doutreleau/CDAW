2f3490415d341518671bc5db1c7c4b5df6b963c8
Doutreleau <56396584+Doutreleau@users.noreply.github.com>
2022-11-16 12:02:42 +0100

    Update listOfPoke.php

--- listOfPoke.php
+++ listOfPoke.php
@@ -31,54 +31,7 @@
                 <?php
                 $id++;
             }
-            /*
-            $names = array();
-            for ($i=0;$i<(strlen($page)-5);$i++){
-                if ($page[$i]=="n" && $page[$i+1]=="a" && $page[$i+2]=="m" && $page[$i+3]=="e"){
-                    $intNextQuote = findNextQuote($i+8,$page);
-                    printName($i,$intNextQuote,$page);
-                    echo $nextName;
-                    echo "EEEEE";
-                    echo $i;
-                    echo "     ";
-                    echo $intNextQuote;
-                    echo "     ";
-                    echo $page[$i+5];
-                    echo $page[$i+6];
-                    echo $page[$i+7];
-                    echo $page[$i+8];
-                    echo $page[$i+9];
-                    echo $page[$i+10];
-                    echo $page[$i+11];
-                    echo $page[$i+12];
-                    echo $page[$i+13];
-                    echo $page[$i+14];
-                    echo "     ";
-                    echo $page[$intNextQuote-5];
-                    echo $page[$intNextQuote-4];
-                    echo $page[$intNextQuote-3];
-                    echo $page[$intNextQuote-2];
-                    echo $page[$intNextQuote-1];
-                    echo $page[$intNextQuote];
-                    echo "BBBB";
-                  //  echo"\n";
-                }
-            }
-
-            function printName($i,$j,$page){
-                for ($k=$i;$k<$j;$k++){
-                    echo $page[$k];
-                }
-            }
-            
-            function findNextQuote($i,$page){
-                for ($j=$i;$j<strlen($page);$j++){
-                    if($page[$j]=="\""){
-                        return $j;
-                    }
-                }
-            }
-            */
+           
         ?>
     </body>
 </html>
